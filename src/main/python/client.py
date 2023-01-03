9b213902902645664e8fe2eb3b560a3629d94f02
ybalbert <ybalbert@163.com>
2023-01-03 18:46:03 +0800

    1. 替换boto 为boto3 2. 对数据做一些调整

--- src/main/python/client.py
+++ src/main/python/client.py
@@ -3,7 +3,7 @@ import json
 import random
 import time
 
-from boto import kinesis
+import boto3
 
 if len(sys.argv) != 4:
   print("invalid arguments...")
@@ -18,20 +18,17 @@ with open(mock_data_path, 'r') as data_file:
   head = ['vendorid', 'lpep_pickup_datetime', 'lpep_dropoff_datetime', 'trip_distance', 'store_and_fwd_flag']
   data_records = [ dict(zip(head, line.split(','))) for line in lines ]
 
+
+kinesis_client = boto3.client('kinesis',region_name=region_name)
+
 for item_id,dict_item in enumerate(data_records):
   dict_item.update({"id": item_id})
-
-data_len = len(data_records)
-
-kinesis_client = kinesis.connect_to_region(region_name)
-
-while True:
-  idx = int(random.uniform(0, data_len))
-  test_record = data_records[idx]
-  time.sleep(0.1)
+  time.sleep(1)
 
   kinesis_client.put_record(
-    stream_name,
-    json.dumps(test_record), # put_record expects a string
-    str(hash(test_record['id'])) # partition key
+    StreamName=stream_name,
+    Data=json.dumps(dict_item), # put_record expects a string
+    PartitionKey=str(hash(dict_item['id'])) # partition key
   )
+
+  print("produce data record {}".format(dict_item['id']))
